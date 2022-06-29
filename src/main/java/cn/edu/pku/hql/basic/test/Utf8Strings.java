10c88cc52ed1581b6564080aa2bf1fd3b0a7ab61
stiga-huang <huangquanlong@gmail.com>
2022-06-29 09:59:09 +0800

    add some utf8 string tests

--- src/main/java/cn/edu/pku/hql/basic/test/Utf8Strings.java
+++ src/main/java/cn/edu/pku/hql/basic/test/Utf8Strings.java
@@ -8,5 +8,17 @@ public class Utf8Strings {
     s = "hello";
     System.out.println(s.length());
     System.out.println(s.getBytes().length);
+
+    s = "北京市海淀区";
+    int cityCodePoint = '市';  // A single quoted Chinese character. It's value is 24066.
+    int a = 0x5E02;
+    System.out.println(a);
+    System.out.println(s.substring(s.indexOf(cityCodePoint) + 1));
+    System.out.println(s.substring(3));
+
+    s = "grüßen";
+    System.out.println(s.toUpperCase());
+    System.out.println("grüßen".toUpperCase());
+    System.out.println("ὈΔΥΣΣΕΎΣ".toLowerCase());
   }
 }
